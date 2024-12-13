7f9112cc8856d4b0310fd78599815d5a793eebb4
saidamzal <saidamzal@debian-sa.debian-sa>
2024-12-13 20:48:59 +0100

    zalagosta

---
+++ README.md
@@ -0,0 +1,7 @@
+## sdz
+
+<p>In this Example I used Scanner class to show how to read the entry keyboard like:</p>
+
+- [x] string variable
+- [x] int,double,float variable
+ 
---
+++ sdz.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class sdz{
+
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("veuillez saisir un mot :"); 
+String str =sc.nextLine();
+System.out.println("le mot saisit est :"+str);
+
+}
+}
